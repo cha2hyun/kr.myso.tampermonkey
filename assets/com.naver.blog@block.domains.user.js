3ee1a8e9e7cef67dda981561f5e80f33ef69259c
Choi Won <help@myso.kr>
2020-03-24 14:14:27 +0900

    Update com.naver.blog@block.domains.user.js

--- assets/com.naver.blog@block.domains.user.js
+++ assets/com.naver.blog@block.domains.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         네이버 블로그 스팸 차단글 자동화 도구 - 국제 도메인
-// @namespace    https://www.myso.kr/
+// @namespace    https://tampermonkey.myso.kr/
 // @version      0.1
 // @description  네이버 블로그 스팸 차단글 설정에서 손쉽게 모든 국제 도메인을 차단 키워드로 등록 할 수 있습니다.
 // @author       Won Choi
-// @match        https://admin.blog.naver.com/*/config/bloginfo
+// @match        https://admin.blog.naver.com/*
 // @grant        none
 // ==/UserScript==
 
