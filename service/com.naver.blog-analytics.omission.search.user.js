a0510fac1b5ab6b43e3d856cc1953b0d01e203a3
Choi Won <help@myso.kr>
2020-03-25 07:03:00 +0900

    Create com.naver.blog-analytics.omission.search.user.js

---
+++ service/com.naver.blog-analytics.omission.search.user.js
@@ -0,0 +1,49 @@
+// ==UserScript==
+// @name         네이버 블로그 PC 통계 플러스 - 통합검색 노출누락 여부 확인
+// @namespace    https://tampermonkey.myso.kr/
+// @version      1.0.0
+// @updateURL    https://tampermonkey.myso.kr/service/com.naver.blog-analytics.omission.search.user.js
+// @description  네이버 블로그 통계에서 네이버 통합검색 노출 여부 확인이 가능한 버튼을 추가해줍니다.
+// @author       Won Choi
+// @match        https://blog.stat.naver.com/blog/article/*/cv
+// @grant        none
+// ==/UserScript==
+async function main() {
+    const container = document.querySelector('.u_ni_header_wrap > div > .u_ni_info_section > div');
+    container.innerHTML += '<div class="u_ni_btn_group" style="left:0"><div class="u_ni_menu_component u_ni_ext_group"><ul class="u_ni_menu_list" role="menu"></ul></div></div>';
+    const canvas = container.querySelector('.u_ni_ext_group ul.u_ni_menu_list');
+    const button_wrap_a = document.createElement('li'); button_wrap_a.className = 'u_ni_menu_item u_ni_is_active'; canvas.appendChild(button_wrap_a);
+    const button_wrap_b = document.createElement('li'); button_wrap_b.className = 'u_ni_menu_item u_ni_is_active'; canvas.appendChild(button_wrap_b);
+    const button_a = document.createElement('a'); button_a.href = '#'; button_a.innerText = '누락확인'; button_wrap_a.appendChild(button_a);
+    const button_b = document.createElement('a'); button_b.href = '#'; button_b.innerText = '유사확인'; button_wrap_b.appendChild(button_b);
+    function post_info() {
+        const el_title = document.querySelector('h3.u_ni_title, span.u_ni_title');
+		const el_write = document.querySelector('.u_ni_date');
+		const tx_title = el_title && el_title.innerText;
+		const tx_write = el_write && el_write.innerText;
+		const pt_write = /^([0-9]{4}).([0-9]{2}).([0-9]{2})/.exec(tx_write);
+		const qr_write = Array.from(pt_write).slice(1, 4).join('');
+        return { tx_title, tx_write, qr_write };
+    }
+    button_a.onclick = function(e) {
+        e.preventDefault();
+        const info = post_info();
+		const uri = new URL('https://search.naver.com/search.naver');
+		uri.searchParams.set('sm', 'tab_opt');
+		uri.searchParams.set('where', 'nexearch');
+		uri.searchParams.set('query', `"${info.tx_title}"`);
+        uri.searchParams.set('nso', `so:dd,p:from${info.qr_write}to${info.qr_write},a:all`);
+		window.open(uri.toString(), 'u_ni_search');
+    }
+    button_b.onclick = function(e) {
+        e.preventDefault();
+        const info = post_info();
+		const uri = new URL('https://search.naver.com/search.naver');
+		uri.searchParams.set('sm', 'tab_opt');
+		uri.searchParams.set('where', 'nexearch');
+		uri.searchParams.set('query', `${info.tx_title}`);
+		window.open(uri.toString(), 'u_ni_search');
+    }
+}
+function checkForDOM() { return (document.head && document.body) ? main() : requestIdleCallback(checkForDOM); }
+requestIdleCallback(checkForDOM);
