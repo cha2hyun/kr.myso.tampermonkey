ca23ecfe7dc6274e747294b97e29831c49ddd1c6
Choi Won <help@myso.kr>
2020-03-25 18:40:36 +0900

    Update com.naver.blog-content.overlap.search.user.js

--- service/com.naver.blog-content.overlap.search.user.js
+++ service/com.naver.blog-content.overlap.search.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         네이버 블로그 중복문서 검색
 // @namespace    https://tampermonkey.myso.kr/
-// @version      1.0.1
+// @version      1.0.2
 // @updateURL    https://tampermonkey.myso.kr/service/com.naver.blog-content.overlap.search.user.js
 // @description  네이버 블로그에서 내 글의 중복문서/짜집기문서를 쉽게 찾기위한 기능을 추가합니다.
 // @author       Won Choi
@@ -10,9 +10,12 @@
 // ==/UserScript==
 async function main() {
     const container = document.querySelector('.blog2_post_function');
-    container.innerHTML = '<a href="#" class="url pcol2 _returnFalse _transPosition _se3overlapbtn"style="cursor:pointer; margin-right:11px;">중복문서 찾기</a>' + container.innerHTML;
-    const button = container.querySelector('._se3overlapbtn');
-    button.onclick = function(e) {
+    const anchor = document.createElement('a'); anchor.href = '#';
+    anchor.className = 'url pcol2 _returnFalse _transPosition _se3screenshotbtn';
+    anchor.style.cursor = 'pointer';
+    anchor.style.marginRight = '11px';
+    anchor.innerText = '중복문서 찾기';
+    anchor.onclick = function(e) {
         e.preventDefault();
         if(confirm('해당 기능은 본문 내용 일부를 무작위로 추출하여 검색합니다. 계속하시겠습니까?')) {
             const arr = document.querySelector('.se-main-container').innerText.replace(/[\n]+/g, ' ').split(' ');
@@ -24,6 +27,7 @@ async function main() {
             window.open(uri, 'overlap_search');
         }
     }
+    container.prepend(anchor);
 }
 function checkForDOM() { return (document.head && document.body) ? main() : requestIdleCallback(checkForDOM); }
 requestIdleCallback(checkForDOM);
