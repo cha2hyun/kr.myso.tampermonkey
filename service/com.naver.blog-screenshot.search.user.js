0e4724f502cfa8a3beef2009323f263826e4321e
Choi Won <help@myso.kr>
2020-03-26 22:36:46 +0900

    Update com.naver.blog-screenshot.search.user.js

--- service/com.naver.blog-screenshot.search.user.js
+++ service/com.naver.blog-screenshot.search.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         네이버 블로그 검색결과 캡쳐도구
 // @namespace    https://tampermonkey.myso.kr/
-// @version      1.0.2
+// @version      1.0.3
 // @updateURL    https://github.com/myso-kr/kr.myso.tampermonkey/raw/master/service/com.naver.blog-screenshot.search.user.js
 // @description  네이버 블로그에서 발행한 포스팅의 검색결과를 손쉽게 캡쳐하는 도구입니다.
 // @author       Won Choi
