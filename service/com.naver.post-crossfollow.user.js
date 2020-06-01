c08d29629183a150b49a84be6888d29f367b77fd
Choi Won <help@myso.kr>
2020-06-01 21:43:33 +0900

    Create com.naver.post-crossfollow.user.js

---
+++ service/com.naver.post-crossfollow.user.js
@@ -0,0 +1,53 @@
+// ==UserScript==
+// @name         네이버 포스트 맞구독 검증기
+// @namespace    https://tampermonkey.myso.kr/
+// @version      1.0.0
+// @updateURL    https://github.com/myso-kr/kr.myso.tampermonkey/raw/master/service/com.naver.post-crossfollow.user.js
+// @description  네이버 포스트에서 맞구독 상태를 검증합니다.
+// @author       Won Choi
+// @match        *://post.naver.com/my/followingList.nhn?*
+// @match        *://m.post.naver.com/my/followingList.nhn?*
+// @grant        GM_addStyle
+// @require      https://tampermonkey.myso.kr/assets/donation.js
+// ==/UserScript==
+async function search_followers(fromNo = 1, totalCount = 22, result = {}) {
+    const loc = new URL(location.href);
+    const uri = new URL('https://post.naver.com/my/followerListMore.nhn');
+    uri.hostname = location.hostname;
+    uri.searchParams.set('lastRowDateTime', Date.now());
+    uri.searchParams.set('totalCount', totalCount);
+    uri.searchParams.set('followNo', loc.searchParams.get('memberNo'));
+    uri.searchParams.set('fromNo', fromNo);
+    const resp = await fetch(uri.toString(), { mode: 'cors', credentials: 'include' }).then(resp=>resp.text());
+    const json = eval(`(${resp})`), body = document.createElement('div'); body.innerHTML = json.html;
+    const data = Array.from(body.querySelectorAll('li.user_info_item')).map((o) => {
+        const uri = new URL(o.querySelector('a.user_lnk').href);
+        return {
+            id: uri.searchParams.get('memberNo'),
+            name: o.querySelector('strong.post_tit').innerText,
+        };
+    });
+    result = Object.assign({}, result, json); delete result.html; result.data = result.data || [];
+    result.data.push(...data);
+    return json.listCount <= fromNo ? result : search_followers(fromNo + 1, totalCount, result);
+}
+async function draw(json) {
+    draw.timer = clearTimeout(draw.timer);
+    draw.timer = setTimeout(() => {
+        Array.from(document.body.querySelectorAll('li.user_info_item')).map((o) => {
+            const uri = new URL(o.querySelector('a.user_lnk').href);
+            const memberNo = uri.searchParams.get('memberNo');
+            const nickName = o.querySelector('strong.post_tit').innerText;
+            if(!json.data.find(v=>v.id==memberNo)) o.classList.add('selfishes');
+        });
+    });
+}
+async function main() {
+    GM_donation('#el_list_container');
+    GM_addStyle(`.selfishes { background: #ffd900; }`);
+    const json = await search_followers(); draw(json);
+    document.__createElement = document.__createElement || document.createElement;
+    document.createElement = (tagName) => (draw(json), document.__createElement(tagName));
+}
+function checkForDOM() { return (document.body) ? main() : requestIdleCallback(checkForDOM); }
+requestIdleCallback(checkForDOM);
