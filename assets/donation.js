2c3c7f27c31f2ca165cdbc0b56559905e73be91a
Choi Won <help@myso.kr>
2020-06-01 22:03:06 +0900

    Update donation.js

--- assets/donation.js
+++ assets/donation.js
@@ -5,8 +5,9 @@ window.GM_donation = function(container, bottom) {
     container.classList.add('donation-myso');
     container.classList.add(bottom ? 'donation-sticky-b' : 'donation-sticky-t');
     container.addEventListener('click', (e) => {
-        const hasT = (!bottom && e.offsetY < 50);
-        const hasB = ( bottom && e.offsetY > container.offsetHeight - 50);
+        const rect = container.getBoundingClientRect();
+        const hasT = (!bottom && e.clientY < rect.top + 50);
+        const hasB = ( bottom && e.clientY > rect.bottom - 50);
         if(hasT || hasB) window.open('https://blog.myso.kr/', '__blog_myso_kr__');
     });
 }
