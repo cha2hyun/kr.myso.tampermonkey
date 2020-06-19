ce4de6961f15c2e6190093aaa56782d0bb83095a
Choi Won <help@myso.kr>
2020-06-19 15:59:54 +0900

    Update com.naver.in-challenge.rank.user.js

--- service/com.naver.in-challenge.rank.user.js
+++ service/com.naver.in-challenge.rank.user.js
@@ -1,12 +1,13 @@
 // ==UserScript==
 // @name         네이버 인플루언서 홈 키워드 챌린지 순위 확인
 // @namespace    https://tampermonkey.myso.kr/
-// @version      1.0.1
+// @version      1.0.2
 // @updateURL    https://github.com/myso-kr/kr.myso.tampermonkey/raw/master/service/com.naver.in-challenge.rank.user.js
 // @description  네이버 인플루언서 홈에서 키워드 챌린지 순위를 확인합니다.
 // @author       Won Choi
 // @match        https://in.naver.com/*/challenge*
 // @grant        GM_xmlhttpRequest
+// @grant        GM_addStyle
 // ==/UserScript==
 async function main() {
     const urlIdBase = location.pathname.split('/')[1];
@@ -30,10 +31,14 @@ async function main() {
     const elements = Array.from(document.querySelectorAll('[class^="ChallengeHistory__list_hashtag"] a'));
     const keywords = elements.map(el=>el.innerText);
     const rankings = await Promise.all(keywords.map(o=>search_ranking_challenges(o)));
+    GM_addStyle(`
+        .Keyword__rank { display: inline-block; overflow: hidden; vertical-align: top; margin-right: -15px; z-index: 1; position: relative; }
+        .Keyword__rank .count { display: block; padding: 9px 15px 8px; border-radius: 18px; background: rgba(255,0,0,.6); font-size: 12px; font-weight: 600; line-height: 18px; letter-spacing: -.5px; color: hsla(0,0%,100%,.7); }
+    `);
     elements.map((el, i) => {
-        el.style.position = 'relative';
-        el.style.overflow = 'auto';
-        el.innerHTML = `<em style="display:inline-block; background-color:red; border-radius:50%; width:28px; height: 28px; overflow: hidden; text-align:center; line-height: 28px;">${rankings[i]?rankings[i].ranking:'100+'}</em> ${keywords[i]} `
+        const em = document.createElement('li'); em.className = 'Keyword__rank';
+        em.innerHTML = `<em class="count">${rankings[i]?rankings[i].ranking:'100+'}</em>`;
+        el.parentNode.parentNode.insertBefore(em, el.parentNode);
         el.addEventListener('click', (e)=>el.getAttribute('aria-pressed')=="true"&&window.open(`https://s.search.naver.com/p/influencer/search.naver?query=${encodeURIComponent(keywords[i])}`), false);
     });
 }
