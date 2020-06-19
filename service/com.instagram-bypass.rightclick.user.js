b7616282d2efff3aef62c126957e10b0e328eaf5
Choi Won <help@myso.kr>
2020-06-20 01:48:02 +0900

    Update com.instagram-bypass.rightclick.user.js

--- service/com.instagram-bypass.rightclick.user.js
+++ service/com.instagram-bypass.rightclick.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         인스타그램 이미지 우클릭 제한 해제
 // @namespace    https://tampermonkey.myso.kr/
-// @version      1.0.3
+// @version      1.0.4
 // @updateURL    https://github.com/myso-kr/kr.myso.tampermonkey/raw/master/service/com.instagram-bypass.rightclick.user.js
 // @description  인스타그램에서 이미지의 우클릭 기능이 활성화됩니다.
 // @author       Won Choi
@@ -10,8 +10,10 @@
 // @match        *://www.instagram.com
 // @match        *://www.instagram.com/*
 // @grant        GM_addStyle
+// @require      https://tampermonkey.myso.kr/assets/donation.js
 // ==/UserScript==
 async function main() {
+    GM_donation('#react-root section', 1);
     GM_addStyle(`
       ._9AhH0 { display: none; }
       .PyenC, .fXIG0 { margin: auto; left: 0; right: 0; top: 0; bottom: 0; width: 135px; height: 135px; }
