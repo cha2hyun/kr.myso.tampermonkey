1121a81898f2ab9c9c0d949104e6889983c0a3b9
Choi Won <help@myso.kr>
2020-06-20 20:25:26 +0900

    Update donation.js

--- assets/donation.js
+++ assets/donation.js
@@ -1,7 +1,7 @@
 window.GM_addStyle = window.GM_addStyle || (()=>{});
 window.GM_addStyle(`@import url('https://tampermonkey.myso.kr/assets/donation.css?z=${Date.now()}')`);
 window.GM_donation = function(container, bottom) {
-    container = (container instanceof Element) ? container : document.querySelector(container);
+    container = (container instanceof Element) ? container : document.querySelector(container); if(!container) return;
     container.classList.add('donation-myso');
     container.classList.add(bottom ? 'donation-sticky-b' : 'donation-sticky-t');
     container.addEventListener('click', (e) => {
