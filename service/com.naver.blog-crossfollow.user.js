6f4bae2cdda1daac74171350f34554aea79cdc0d
Choi Won <help@myso.kr>
2020-06-20 20:40:45 +0900

    Update com.naver.blog-crossfollow.user.js

--- service/com.naver.blog-crossfollow.user.js
+++ service/com.naver.blog-crossfollow.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name         네이버 블로그 나만 이웃 자동 정리
 // @namespace    https://tampermonkey.myso.kr/
-// @version      1.0.3
+// @version      1.0.4
 // @updateURL    https://github.com/myso-kr/kr.myso.tampermonkey/raw/master/service/com.naver.blog-crossfollow.user.js
 // @description  네이버 블로그에 나만 이웃 중인 이웃을 자동으로 정리해줍니다.
 // @author       Won Choi
 // @match        https://admin.blog.naver.com/*
-// @require      https://tampermonkey.myso.kr/assets/donation.js
+// @require      https://tampermonkey.myso.kr/assets/donation.js?v=1
 // @grant        GM_addStyle
 // ==/UserScript==
 async function search_buddy_me_page(page = 1, results = []) {
