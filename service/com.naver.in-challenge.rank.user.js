13ef15eea54ac99290d93e9d47f45db8a6d87fcd
Choi Won <help@myso.kr>
2020-06-24 02:58:52 +0900

    Update com.naver.in-challenge.rank.user.js

--- service/com.naver.in-challenge.rank.user.js
+++ service/com.naver.in-challenge.rank.user.js
@@ -1,14 +1,14 @@
 // ==UserScript==
 // @name         네이버 인플루언서 홈 키워드 챌린지 순위 확인
 // @namespace    https://tampermonkey.myso.kr/
-// @version      1.0.5
+// @version      1.0.6
 // @updateURL    https://github.com/myso-kr/kr.myso.tampermonkey/raw/master/service/com.naver.in-challenge.rank.user.js
 // @description  네이버 인플루언서 홈에서 키워드 챌린지 순위를 확인합니다.
 // @author       Won Choi
 // @match        https://in.naver.com/*/challenge*
 // @grant        GM_xmlhttpRequest
 // @grant        GM_addStyle
-// @require      https://tampermonkey.myso.kr/assets/donation.js?v=2
+// @require      https://tampermonkey.myso.kr/assets/donation.js?v=3
 // ==/UserScript==
 async function main() {
     main.timer = clearTimeout(main.timer);
