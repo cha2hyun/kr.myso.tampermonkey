dd075b1d68605acc61933303efcf5b2d38ae7e10
Choi Won <help@myso.kr>
2020-11-21 17:40:01 +0900

    no message

--- assets/donation.js
+++ assets/donation.js
@@ -1,16 +1,20 @@
-window.GM_addStyle = window.GM_addStyle || (function(){});
-window.GM_addStyle(`@import url('https://tampermonkey.myso.kr/assets/donation.css?z=${Date.now()}')`);
-window.GM_donation = function(container, bottom) {
-    container = (container instanceof Element) ? container : document.querySelector(container);
-    if(container) {
-        container.classList.add('donation-myso');
-        container.classList.add(bottom ? 'donation-sticky-b' : 'donation-sticky-t');
-        container.addEventListener('click', (e) => {
-            if(e.target != container) return;
-            const rect = container.getBoundingClientRect();
-            const hasT = (!bottom && e.clientY < rect.top + 50);
-            const hasB = ( bottom && e.clientY > rect.bottom - 50);
-            if(hasT || hasB) window.open('https://blog.myso.kr/', '__blog_myso_kr__');
+(function(window) {
+    window.GM_addStyle = window.GM_addStyle || (function(){});
+    window.GM_addStyle(`@import url('https://tampermonkey.myso.kr/assets/donation.css?z=${Date.now()}')`);
+    window.GM_donation = function(container, bottom) {
+        setTimeout(function() {
+            container = (container instanceof Element) ? container : document.querySelector(container);
+            if(container) {
+                container.classList.add('donation-myso');
+                container.classList.add(bottom ? 'donation-sticky-b' : 'donation-sticky-t');
+                container.addEventListener('click', (e) => {
+                    if(e.target != container) return;
+                    const rect = container.getBoundingClientRect();
+                    const hasT = (!bottom && e.clientY < rect.top + 50);
+                    const hasB = ( bottom && e.clientY > rect.bottom - 50);
+                    if(hasT || hasB) window.open('https://blog.myso.kr/', '__blog_myso_kr__');
+                });
+            }
         });
     }
-}
+})(window);
