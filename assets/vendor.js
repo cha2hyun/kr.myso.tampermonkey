e9b68274a5d14c1726d257e2329a164ef81529a2
Choi Won <help@myso.kr>
2020-11-21 17:56:40 +0900

    no message

--- assets/vendor.js
+++ assets/vendor.js
@@ -98,3 +98,71 @@
     window.__loaded__ = true;
   }, 1000);
 })(window);
+// ---------------------
+(function () {
+  Object.pick = function(object, ...filter) {
+    let keys = Object.keys(object).filter((key)=>filter.includes(key));
+    return keys.reduce((out, key)=>(out[key] = object[key], out), {});
+  }
+  Object.omit = function(object, ...filter) {
+    let keys = Object.keys(object).filter((key)=>!filter.includes(key));
+    return keys.reduce((out, key)=>(out[key] = object[key], out), {});
+  }
+  Object.conservation = function(object, ...filter) {
+    let keys = Object.keys(object).filter((key)=>!filter.includes(key));
+    keys.map((key)=>delete object[key]);
+  }
+  Object.exclusion = function(object, ...filter) {
+    let keys = Object.keys(object).filter((key)=>filter.includes(key));
+    keys.map((key)=>delete object[key]);
+  }
+  Array.prototype.excludes = function(x) { return !this.includes(x); }
+  function ArrayClass(Clazz) {
+    let prototypeArray = Object.getOwnPropertyNames(Array.prototype);
+    let prototypeClazz = Object.getOwnPropertyNames(Clazz.prototype);
+    let prototype = prototypeArray.filter(prototypeClazz.excludes.bind(prototypeClazz)); prototype.push(Symbol.iterator, Symbol.unscopables);
+    prototype.map(prototypeName=>{
+      if(typeof Clazz.prototype[prototypeName] !== 'undefined') return;
+      Clazz.prototype[prototypeName] = Array.prototype[prototypeName];
+    });
+  }
+  ArrayClass(NodeList);
+  ArrayClass(DOMTokenList);
+  ArrayClass(HTMLCollection);
+  DOMTokenList.prototype.startsWith = function(className) { return this.find(o=>o.startsWith(className)); }
+  DOMTokenList.prototype.endsWith = function(className) { return this.find(o=>o.endsWith(className)); }
+})();
+// ---------------------
+(function (window) {
+  HTMLDocument.prototype.createElement = ((createElement) => {
+    return function() {
+      createElement.name = 'createElement';
+      let element = createElement.apply(this, arguments);
+      return element;
+    }
+  })(HTMLDocument.prototype.createElement$ = HTMLDocument.prototype.createElement$ || HTMLDocument.prototype.createElement);
+  HTMLElement.prototype.appendChild = ((appendChild) => {
+    return function() {
+      appendChild.name = 'appendChild';
+      let element = handleElement.bind(this, appendChild).apply(this, arguments);
+      handleElementRecursive(element);
+      return element;
+    }
+  })(HTMLElement.prototype.appendChild$ = HTMLElement.prototype.appendChild$ || HTMLElement.prototype.appendChild);
+
+  function handleElementRecursive(element, ...props) {
+    if(!element) return;
+    handleElement.call(element, null, element);
+    if(window.handleElementRecursive) {
+      element = window.handleElementRecursive.apply(this, arguments);
+    }
+    return element;
+  }
+  function handleElement(handler, element, ...props) {
+    if(!element) return;
+    if(window.handleElement) {
+      element = window.handleElement.apply(this, arguments);
+    }
+    return element;
+  }
+})(window);
