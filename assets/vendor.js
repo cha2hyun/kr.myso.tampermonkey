8957887be5601affeefd5c38685d2a94b5ea4c7d
Choi Won <help@myso.kr>
2020-11-21 18:43:34 +0900

    no message

--- assets/vendor.js
+++ assets/vendor.js
@@ -140,32 +140,21 @@
       return createElement.apply(this, arguments);
     }
   })(HTMLDocument.prototype.createElement$ = HTMLDocument.prototype.createElement$ || HTMLDocument.prototype.createElement);
+
   HTMLElement.prototype.appendChild = ((appendChild) => {
     return function() {
       appendChild.name = 'appendChild';
-      return handleElementRecursive.bind(this, appendChild).apply(this, arguments);
+      handleElementRecursive.apply(this, arguments);
+      return appendChild.apply(this, arguments);
     }
   })(HTMLElement.prototype.appendChild$ = HTMLElement.prototype.appendChild$ || HTMLElement.prototype.appendChild);
-  HTMLElement.prototype.removeChild = ((removeChild) => {
-    return function() {
-      removeChild.name = 'removeChild';
-      try { return removeChild.apply(this, arguments); } catch(e) {}
-    }
-  })(HTMLElement.prototype.removeChild$ = HTMLElement.prototype.removeChild$ || HTMLElement.prototype.removeChild);
 
-  function handleElementRecursive(handler, element, ...props) {
+  function handleElementRecursive(element, ...props) {
     if(element && element instanceof HTMLElement) {
-      element = handleElement.apply(this, arguments);
       if(window.handleElementRecursive) window.handleElementRecursive.apply(this, arguments);
       if(element instanceof HTMLElement && element.children) {
         element.children.filter((child)=>child instanceof HTMLElement).map((child)=>handleElementRecursive.call(element, null, child));
       }
-    }
-    return element;
-  }
-  function handleElement(handler, element, ...props) {
-    if(element && element instanceof HTMLElement && window.handleElement) {
-      element = window.handleElement.apply(this, arguments) || element;
     }
     return element;
   }
