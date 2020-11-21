cad6a633deea867a404a1ab6870852e4fa662ef4
Choi Won <help@myso.kr>
2020-11-21 16:48:44 +0900

    no message

--- assets/vendor.js
+++ assets/vendor.js
@@ -1,3 +1,44 @@
+// ---------------------
+((window) => {
+  window.__queueDelay = 1000 * 15;
+  window.__queueTimer;
+  window.__queues = [];
+  window.setTimeoutQueue = function(handler, timeout = window.__queueDelay) { window.__queues.push({ handler, timeout }); }
+  window.clearTimeoutQueue = function(){
+    window.__queueTimer = clearTimeout(window.__queueTimer);
+    window.__queues.splice(0, window.__queues.length - 1);
+    window.runTimeoutQueue();
+  }
+  window.runTimeoutQueue = function() {
+    window.__queueTimer = clearTimeout(window.__queueTimer);
+    window.__queueTimer = setTimeout(() => {
+      window.__queueTimer = clearTimeout(window.__queueTimer);
+      let now = Date.now();
+      let queue = window.__queues.shift();
+      let timeout = queue ? (queue.timeout || window.__queueDelay) : window.__queueDelay;
+      if(queue && queue.handler) { queue.handler.apply(this); }
+      let dif = Date.now() - now;
+      let handle = setTimeout(() => {
+          handle = clearTimeout(handle);
+          window.runTimeoutQueue();
+      }, Math.max(window.__queueDelay, timeout - dif));
+    });
+  }
+  window.runTimeoutQueue();
+})(window);
+// ---------------------
+((window) => {
+  window.inject_js = function inject_js(script) {
+    const container = (document.head || document.body || document.documentElement);
+    const element = document.createElement('script');
+    element.setAttribute('type', 'text/javascript');
+    element.textContent = `(${script})()`;
+    container.append(element);
+    element.onload = () => container.removeChild(element);
+    setTimeout(()=>container.removeChild(element), 300);
+  }
+})(window);
+// ---------------------
 ((window) => {
   const delay = 300;
   const main = { timer: null };
