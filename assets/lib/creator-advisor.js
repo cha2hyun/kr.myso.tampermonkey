a4ef6e74f5f3d2c345671989229c90a7a27b9466
Choi Won <help@myso.kr>
2020-12-09 10:12:49 +0900

    no message

--- assets/lib/creator-advisor.js
+++ assets/lib/creator-advisor.js
@@ -15,11 +15,19 @@
       constructor() {
           this.commands = {};
           this.command('channels', '/accounts/channels');
+          this.command('revenue-summary', '/home/revenue-summary');
+          this.command('cv-summary', '/home/cv-summary');
+          
+          this.command('query-compare', '/inflow-analysis/query-compare?service=&channelId=&metric=cv&interval=day&date=&contentType=text&limit=10');
           this.command('popular-contents', '/inflow-analysis/popular-contents?service=&channelId=&metric=cv&contentType=text&interval=day&date=&limit=5&keyword=');
+          this.command('referrer-query-trends', '/inflow-analysis/referrer-query-trends?service=&channelId=&metric=cv&interval=day&startDate=&endDate=&queries=&contentId=&ct=');
+          this.command('referrer-query-summary', '/inflow-analysis/referrer-query-summary?service=&channelId=&metric=cv&interval=day&date=&limit=');
+
           this.command('view-count', '/integrated-analysis/view-count?service=&channelId=&interval=day&startDate=&endDate=&contentId=');
+          this.command('channel-ranks', '/integrated-analysis/channel-ranks?service=&channelId=&interval=day&date=&limit=20&keywordCount=5');
           this.command('referrer-query', '/integrated-analysis/referrer-query?service=&channelId=&metric=cv&interval=day&date=&limit=2&referrerDomain=&etc=&etcSize=&contentId=');
           this.command('content-demo-trend', '/integrated-analysis/content-demo-trend?service=&channelId=&metric=cv&interval=day&startDate=&endDate=&contentId=');
-          this.command('referrer-query-trends', '/inflow-analysis/referrer-query-trends?service=&channelId=&metric=cv&interval=day&startDate=&endDate=&queries=&contentId=&ct=');
+          
       }
       command(name, endpoint, defaults) { return this.commands[name] = new CreatorAdvisorApi(endpoint, defaults || {}); }
       exec(name, data) {
