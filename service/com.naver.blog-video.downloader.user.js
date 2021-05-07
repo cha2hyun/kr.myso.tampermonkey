bb69caa94742faa760eda7daff54a7c9b44622af
Choi Won <help@myso.kr>
2021-05-07 14:06:33 +0900

    no message

--- service/com.naver.blog-video.downloader.user.js
+++ service/com.naver.blog-video.downloader.user.js
@@ -1,11 +1,16 @@
 // ==UserScript==
 // @name         네이버 블로그 동영상 다운로더
 // @namespace    https://tampermonkey.myso.kr/
-// @version      1.0.1
+// @version      1.1.0
 // @updateURL    https://github.com/myso-kr/kr.myso.tampermonkey/raw/master/service/com.naver.blog-video.downloader.user.js
 // @description  네이버 블로그 동영상 다운로드 기능을 활성화됩니다.
 // @author       Won Choi
+// @grant        GM_addStyle
+// @match        https://blog.naver.com/PostView.nhn?*
+// @match        https://blog.naver.com/PostList.nhn?*
 // @match        *://serviceapi.nmv.naver.com/flash/convertIframeTag.nhn?*
+// @require      https://tampermonkey.myso.kr/assets/donation.js?v=3
+// @require      https://cdnjs.cloudflare.com/ajax/libs/toastify-js/1.11.0/toastify.min.js
 // ==/UserScript==
 async function inject_xhr() {
     const XHR = XMLHttpRequest.prototype, send = XHR.send, open = XHR.open;
@@ -17,11 +22,13 @@ async function inject_xhr() {
             var event = new CustomEvent('xhrload', { detail });
             window.dispatchEvent(event);
         });
-        this.addEventListener('error', () => alert(`${this.status} 에러\n네이버에서 통신오류가 발생하였습니다 잠시 후 다시 시도해주세요.`));
+        //this.addEventListener('error', () => alert(`${this.status} 에러\n네이버에서 통신오류가 발생하였습니다 잠시 후 다시 시도해주세요.`));
         send.apply(this, arguments);
     };
 };
 async function main() {
+    GM_donation('#viewTypeSelector, #postListBody, #wrap_blog_rabbit, #writeTopArea, #editor_frame', 0);
+    GM_addStyle("@import url('https://cdnjs.cloudflare.com/ajax/libs/toastify-js/1.11.0/toastify.min.css')");
     inject_xhr();
     window.addEventListener('xhrload', (e) => {
         const { url, response } = e.detail;
@@ -29,10 +36,14 @@ async function main() {
             const info = JSON.parse(response);
             const title = info.meta.subject;
             const video = info.videos.list.reduce((r, o)=>r.size < o.size ? o : r, { size: 0 });
-            location.href = video.source;
+            const toast = Toastify({
+                duration: 1000 * 60,
+                text: `${title}\n첨부 영상이 발견 됨 (새 창으로 열기)`,
+                onClick: () => window.open(video.source),
+            }).showToast();
         }
     }, false);
-    loadUGCPlayer();
+    if(typeof loadUGCPlayer !== 'undefined') loadUGCPlayer();
 }
 function _requestIdleCallback(callback) {
     if(typeof requestIdleCallback == 'undefined') return setTimeout(callback, 1000);
