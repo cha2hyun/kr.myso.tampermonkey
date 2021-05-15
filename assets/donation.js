cbe9ddbb7f6b90ef90bb165c57b3efd72fbcf6e4
Choi Won <help@myso.kr>
2021-05-15 12:44:37 +0900

    no message

--- assets/donation.js
+++ assets/donation.js
@@ -1,31 +1,34 @@
 (function(window) {
     window.GM_addStyle = window.GM_addStyle || (function(){});
     window.GM_addStyle(`@import url('https://tampermonkey.myso.kr/assets/donation.css?z=${Date.now()}')`);
-    window.GM_donation = function(container, bottom) {
+    window.GM_donation = function(container) {
         container = (container instanceof Element) ? container : document.querySelector(container);
         if(container) {
             container.classList.add('donation-myso');
-            container.classList.add(bottom ? 'donation-sticky-b' : 'donation-sticky-t');
+            container.classList.add('donation-sticky-t');
             container.addEventListener('click', (e) => {
                 if(e.target != container) return;
-                const rect = container.getBoundingClientRect();
-                const hasT = (!bottom && e.clientY < rect.top + 50);
-                const hasB = ( bottom && e.clientY > rect.bottom - 50);
-                // if(hasT || hasB) window.open('https://blog.myso.kr/', '__blog_myso_kr__');
-                if(hasT || hasB) window.open('https://in.naverpp.com/donation', '__blog_myso_kr__');
+                let rect = container.getBoundingClientRect();
+                let hasT = e.clientY < rect.top + 50;
+                if(hasT) window.open('https://in.naverpp.com/donation', '__blog_myso_kr__');
             });
+
+            let iframe = document.querySelector('donation-myso-frame') || document.createElement('iframe');
+            iframe.classList.add('donation-myso-frame');
+            iframe.setAttribute('src', 'https://in.naverpp.com/ad/randomize?display');
+            container.prepend(iframe);
         }
         // GA
-        var header = document.querySelector('head');
+        let header = document.querySelector('head');
         if(header) {
-            var js = document.querySelector('#donation-myso-js') || document.createElement('script');
+            let js = document.querySelector('#donation-myso-js') || document.createElement('script');
             js.id = 'donation-myso-js';
             js.textContent = "window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-QPY3HB7Y3Z');";
             header.prepend(js);
 
-            var ga = document.querySelector('#donation-myso-ga') || document.createElement('script');
+            let ga = document.querySelector('#donation-myso-ga') || document.createElement('script');
             ga.id = 'donation-myso-ga'; ga.async = 'async'; ga.src = 'https://www.googletagmanager.com/gtag/js?id=G-QPY3HB7Y3Z';
-            header.prepend(ga);          
+            header.prepend(ga);
         }
     }
 })(window);
