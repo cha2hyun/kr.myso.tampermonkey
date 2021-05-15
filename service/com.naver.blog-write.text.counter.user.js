46e0f5e39c62cbccdbd9afd10fe7adacec8add8c
Choi Won <help@myso.kr>
2021-05-15 12:53:25 +0900

    no message

--- service/com.naver.blog-write.text.counter.user.js
+++ service/com.naver.blog-write.text.counter.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         네이버 블로그&포스트 글자수 세기
 // @namespace    https://tampermonkey.myso.kr/
-// @version      1.0.24
+// @version      1.0.25
 // @updateURL    https://github.com/myso-kr/kr.myso.tampermonkey/raw/master/service/com.naver.blog-write.text.counter.user.js
 // @description  네이버 블로그&포스트에서 글자수 세기를 활성화합니다.
 // @author       Won Choi
@@ -21,7 +21,7 @@
 // @require      https://tampermonkey.myso.kr/assets/donation.js?v=3
 // ==/UserScript==
 async function main() {
-    GM_donation('#viewTypeSelector, #postListBody, #wrap_blog_rabbit, #writeTopArea, #editor_frame', 0);
+    GM_donation('.blog_editor, #viewTypeSelector, #postListBody, #wrap_blog_rabbit, #writeTopArea, #editor_frame', 0);
     GM_addStyle(`
 head { display: block !important; }
 .se-toast-popup.content-length {
