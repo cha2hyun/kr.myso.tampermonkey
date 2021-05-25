fd4a5c24da5ad57013406d30dd0a28d49a00c188
Choi Won <help@myso.kr>
2021-05-25 22:09:31 +0900

    no message

--- service/com.naver.search-text.counter.user.js
+++ service/com.naver.search-text.counter.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         네이버 검색결과 블로그&포스트 글자수 세기
 // @namespace    https://tampermonkey.myso.kr/
-// @version      1.0.0
+// @version      1.0.1
 // @updateURL    https://github.com/myso-kr/kr.myso.tampermonkey/raw/master/service/com.naver.search-text.counter.user.js
 // @description  네이버 검색결과에서 블로그&포스트 글자수 세기를 활성화합니다.
 // @author       Won Choi
@@ -55,6 +55,7 @@ async function contentLength(target) {
   return { contentLengthTxt, contentLengthTrimTxt }
 }
 async function parse(target) {
+  if(!target || !target.querySelector) return;
   const anchor = target.querySelector('a.total_tit[href*="blog.naver.com"]'); if(!anchor) return;
   const uri = new URL(anchor.href); uri.hostname = 'm.blog.naver.com';
   const res = await fetch(uri.toString()).then(r=>r.text());
