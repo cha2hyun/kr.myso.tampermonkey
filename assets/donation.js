aedab5100f6c23e045247aff03b02c30443c3842
Choi Won <help@myso.kr>
2021-05-31 10:13:09 +0900

    no message

--- assets/donation.js
+++ assets/donation.js
@@ -4,6 +4,20 @@
     window.GM_addStyle = window.GM_addStyle || (function(){});
     window.GM_xmlhttpRequest = window.GM_xmlhttpRequest || (function(){});
     window.GM_addStyle(`@import url('https://tampermonkey.myso.kr/assets/donation.css?z=${Date.now()}')`);
+    window.GM_detectAdBlock = function(callback) {
+        async function detectAdBlock() {
+            let adBlockEnabled = false
+            let googleAdUrl = 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js'
+            try {
+                await fetch(new Request(googleAdUrl)).catch(_ => adBlockEnabled = true)
+            } catch (e) {
+                adBlockEnabled = true
+            } finally {
+                return adBlockEnabled;
+            }
+        }
+        return detectAdBlock().then(callback);
+    }
     window.GM_donation = function(container) {
         container = (container instanceof Element) ? container : document.querySelector(container);
         if(container) {
@@ -15,18 +29,26 @@
             // events
             container.addEventListener('click', (e) => {
                 if(e.target != container) return;
+                let full = false;
+                full = full || container.classList.contains('donation-myso-fullscreen');
+                full = full || container.classList.contains('donation-myso-adblock');
+
                 let rect = container.getBoundingClientRect();
-                let size = container.classList.contains('donation-myso-fullscreen') ? rect.height : 50, over = e.clientY < rect.top + size;
+                let size = full ? rect.height : 50, over = e.clientY < rect.top + size;
                 if(over) window.open('https://in.naverpp.com/donation', '__blog_myso_kr__');
                 // fullscreen
                 container.classList.toggle('donation-myso-fullscreen', 0);
                 localStorage.setItem(key.fullscreen_timestamp, Date.now());
             });
-            // adframe
             let iframe = document.querySelector('.donation-myso-frame') || document.createElement('iframe');
             iframe.classList.add('donation-myso-frame');
             iframe.setAttribute('src', 'https://in.naverpp.com/ad/randomize?display');
             container.prepend(iframe);
+            window.GM_detectAdBlock((adBlockEnabled) => {
+                if(adBlockEnabled) { container.innerHTML = ''; }
+                console.log(`AdBlock Enabled: ${adBlockEnabled}`);
+                container.classList.toggle('donation-myso-adblock', adBlockEnabled);
+            });
         }
         // GA
         let header = document.querySelector('head');
