e70c93b501a94a051ecd8eae64c73bf1216ffa59
Choi Won <help@myso.kr>
2021-06-01 06:00:39 +0900

    no message

--- service/com.naver.search-trend.analysis.user.js
+++ service/com.naver.search-trend.analysis.user.js
@@ -1,13 +1,14 @@
 // ==UserScript==
 // @name         네이버 검색결과 데이터랩 검색어 트렌드
 // @namespace    https://tampermonkey.myso.kr/
-// @version      1.0.0
+// @version      1.0.1
 // @updateURL    https://github.com/myso-kr/kr.myso.tampermonkey/raw/master/service/com.naver.search-trend.analysis.user.js
 // @description  네이버 검색결과에서 데이터랩의 검색어 트렌드 정보를 확인할 수 있습니다.
 // @author       Won Choi
 // @match        *://search.naver.com/search.naver?*
 // @match        *://m.search.naver.com/search.naver?*
 // @connect      naver.com
+// @connect      ryo.co.kr
 // @grant        GM_addStyle
 // @grant        GM_xmlhttpRequest
 // @require      https://cdn.jsdelivr.net/npm/chart.js
@@ -15,6 +16,8 @@
 // @require      https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.21/lodash.min.js
 // @require      https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.1/moment.min.js
 // ==/UserScript==
+function format_number(number) { return number.toString().split( /(?=(?:\d{3})+(?:\.|$))/g ).join( "," ); }
+function parsed_number(number) { return /^[\d\.]+$/.test(String(number)) ? parseFloat(number) : 0; }
 function json_parse(text) { try { return JSON.parse(text); } catch(e) { return text; } }
 async function request(url, options = { method: 'GET' }) { return new Promise((resolve, reject) => { GM_xmlhttpRequest(Object.assign({ method: 'GET', url: url.toString(), onerror: reject, onload: resolve, }, options)); }); }
 async function get_hash_keyword_trend(keyword, options = { gender: '', age: '', device: '' }) {
@@ -57,24 +60,65 @@ async function get_keyword_trend(keyword, options = {}) {
         }
     }
 }
+async function get_keyword_count(keyword) {
+    const uri = new URL('https://www.ryo.co.kr/naver/keyword?position=main&callback=update_keyword_analysis&dn=&keyword='); uri.searchParams.set('keyword', keyword);
+    const res = await request(uri.toString());
+    function update_keyword_analysis(data){
+        const resp = {}; if(!data) return;
+        resp.monthlyPcQcCnt = parsed_number(data && data.monthlyPcQcCnt);
+        resp.monthlyMobileQcCnt = parsed_number(data && data.monthlyMobileQcCnt);
+        resp.monthlyQcCnt = resp.monthlyPcQcCnt + resp.monthlyMobileQcCnt;
+        return resp;
+    }
+    return eval(res.responseText);
+}
+async function data_normalize(keyword) {
+    const stat = await get_keyword_count(keyword);
+    const data = await get_keyword_trend(keyword);
+    const sums = data.reduce((r, o)=>r+o.value, 0);
+    const tick = stat ? (stat.monthlyQcCnt / sums) : 1;
+    const items = data.map((item)=>(item.value = (item.value * tick), item));
+    return { items, rval: !!stat };
+}
 async function main() {
     GM_donation('#container', 0);
     const keyword = (new URL(location.href)).searchParams.get('query'); if(!keyword) return;
-    const data = await get_keyword_trend(keyword);
+    const data = await data_normalize(keyword);
     const pack = document.querySelector('#main_pack, #snb');
     const wrap = pack.querySelector('.section.trend') || document.createElement('section'); wrap.classList.add('section', 'trend'); pack.prepend(wrap);
-    const canv = wrap.querySelector('canvas') || document.createElement('canvas'); wrap.append(canv);
-    canv.style.width = '100%';
-    canv.style.height = '120px';
+    const canv = wrap.querySelector('canvas') || document.createElement('canvas'); canv.style.width = '100%'; canv.style.height = '120px'; wrap.append(canv);
     const config = {
         type: 'line',
         data: {
-            labels: _.map(data, o=>o.period),
-            datasets: [{ backgroundColor: '#74d2e7', borderColor: '#48a9c5', data: _.map(data, o=>o.value), }],
+            labels: _.map(data.items, o=>o.period),
+            datasets: [{ backgroundColor: '#74d2e7', borderColor: '#48a9c5', data: _.map(data.items, o=>o.value), }],
         },
         options: {
             scales: { x: { display: false, } },
-            plugins: { title: { display: true, text: '검색어 트렌드' }, legend: { display: false }, },
+            plugins: {
+                title: {
+                    display: true,
+                    text(context) {
+                        if(data.rval) {
+                            return '검색어 트렌드 (실 검색량)';
+                        } else {
+                            return '검색어 트렌드';
+                        }
+                    }
+                },
+                legend: { display: false },
+                tooltip: {
+                    callbacks: {
+                        label(context) {
+                            if(data.rval) {
+                                return `${format_number(context.parsed.y.toFixed(0))}회 검색 됨`
+                            } else {
+                                return `${format_number(context.parsed.y.toFixed(2))}%`
+                            }
+                        }
+                    }
+                }
+            },
         }
     };
     const chart = new Chart(canv, config);
