c7284b630198022f803cb72ca44df40eec9310c1
Choi Won <help@myso.kr>
2021-06-08 12:13:57 +0900

    no message

--- service/com.naver.blog-write.text.counter.user.js
+++ service/com.naver.blog-write.text.counter.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         네이버 블로그&포스트 글자수 세기
 // @namespace    https://tampermonkey.myso.kr/
-// @version      1.0.27
+// @version      1.1.0
 // @updateURL    https://github.com/myso-kr/kr.myso.tampermonkey/raw/master/service/com.naver.blog-write.text.counter.user.js
 // @description  네이버 블로그&포스트에서 글자수 세기를 활성화합니다.
 // @author       Won Choi
@@ -18,9 +18,14 @@
 // @match        *://m.post.naver.com/viewer/postView.nhn?*
 // @match        *://blog.naver.com/lib/smarteditor2/*/smart_editor2_inputarea.html
 // @grant        GM_addStyle
-// @require      https://tampermonkey.myso.kr/assets/donation.js?v=3
+// @require      https://tampermonkey.myso.kr/assets/vendor/gm-app.js
+// @require      https://tampermonkey.myso.kr/assets/vendor/gm-add-style.js
+// @require      https://tampermonkey.myso.kr/assets/vendor/gm-add-script.js
+// @require      https://tampermonkey.myso.kr/assets/donation.js?v=5
+// @require      https://tampermonkey.myso.kr/assets/lib/smart-editor-one.js?v=16
 // ==/UserScript==
-async function main() {
+// ---------------------
+GM_App(async function main() {
     GM_donation('#viewTypeSelector, #postListBody, #wrap_blog_rabbit, #writeTopArea, #editor_frame', 0);
     GM_addStyle(`
 head { display: block !important; }
@@ -77,47 +82,9 @@ head { display: block !important; }
     color: #0e86fb
 }
     `);
-
     function handler(e) {
-        const sections_v2 = Array.from(document.querySelectorAll('#postViewArea, body.se2_inputarea')).map((component) => {
-            const filter = ['se-toast-popup', '__se_object', 'og', '_naverVideo'];
-            function textContents(output, element) {
-                if(filter.find(o=>element.className && element.className.includes(o))) return output;
-                const textNodes = Array.from(element.childNodes).filter(o=>o instanceof Text);
-                const selements = Array.from(element.childNodes).filter(o=>o instanceof HTMLElement);
-                output = selements.reduce(textContents, output);
-                output.push(...textNodes);
-                return output;
-            }
-            const section = {};
-            const data = Array.from(component.childNodes).reduce(textContents, []); section.data = data.map(el=>el.textContent || el.value || '');
-            return section;
-        });
-        const sections_v3 = Array.from(document.querySelectorAll('#se_components_wrapper .se_component, .se_component_wrap .se_component, .se_card_container .se_component, .__se_editor-content .se_component, .se-main-container .se-component, .se-container .se-component')).map((component) => {
-            const section = {};
-            const data = Array.from(component.querySelectorAll('.se_textarea, .se-text-paragraph')); section.data = data.map(el=>el.innerText || el.value || '');
-            return section;
-        });
-        const placeholders = Array.from(document.querySelectorAll('.se_textarea, .se-text-paragraph')).map((component) => {
-            const section = {};
-            const data = Array.from(component.querySelectorAll('.se-placeholder')); section.data = data.map(el=>el.innerText || el.value || '');
-            return section;
-        });
-
-        const contentV2Length = Number(sections_v2.reduce((r, o)=>r + (o.data || []).reduce((r,l)=>r+=l.length, 0), 0));
-        const contentV2LengthTrim = Number(sections_v2.reduce((r, o)=>r + (o.data || []).reduce((r,l)=>r+=l.replace(/[\s]+/g, '').length, 0), 0));
-        const contentV3Length = Number(sections_v3.reduce((r, o)=>r + (o.data || []).reduce((r,l)=>r+=l.length, 0), 0));
-        const contentV3LengthTrim = Number(sections_v3.reduce((r, o)=>r + (o.data || []).reduce((r,l)=>r+=l.replace(/[\s]+/g, '').length, 0), 0));
-
-        const contentLength = contentV2Length + contentV3Length;
-        const contentLengthTrim = contentV2LengthTrim + contentV3LengthTrim;
-        const placeholderLength = Number(placeholders.reduce((r, o)=>r + (o.data || []).reduce((r,l)=>r+=l.length, 0), 0));
-        const placeholderLengthTrim = Number(placeholders.reduce((r, o)=>r + (o.data || []).reduce((r,l)=>r+=l.replace(/[\s]+/g, '').length, 0), 0));
-
-        if(!contentLength) return;
-
-        const contentLengthTxt = String(contentLength-placeholderLength).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,');
-        const contentLengthTrimTxt = String(contentLengthTrim-placeholderLengthTrim).toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,');
+        const se = SE_parse(document); if(!se) return;
+        console.log(se);
         const container = document.querySelector('head');
         const se_toast_popup = container.querySelector('.se-toast-popup.content-length') || document.createElement('div');
         const se_toast_popup_container = se_toast_popup.querySelector('.se-toast-popup-contiainer') || document.createElement('div');
@@ -125,15 +92,9 @@ head { display: block !important; }
         const se_toast_popup_message = se_toast_popup_content.querySelector('.se-toast-popup-message') || document.createElement('p');
         if(!se_toast_popup.className) { se_toast_popup.className = 'se-toast-popup se-toast-interaction-enter content-length'; container.append(se_toast_popup); }
         if(!se_toast_popup_container.className) { se_toast_popup_container.className = 'se-toast-popup-contiainer'; se_toast_popup.append(se_toast_popup_container); }
-        if(!se_toast_popup_content.className) {
-            se_toast_popup_content.className = 'se-toast-popup-content se-toast-popup-content-info'; se_toast_popup_container.append(se_toast_popup_content);
-            se_toast_popup_content.addEventListener('click', () => {
-                const content = sections.reduce((r, o)=>(r.push(...o.data),r), []).join('\r\n');
-                prompt('', content.replace(/[\r\n\s\t]+/g, ''));
-            }, false);
-        }
+        if(!se_toast_popup_content.className) { se_toast_popup_content.className = 'se-toast-popup-content se-toast-popup-content-info'; se_toast_popup_container.append(se_toast_popup_content); }
         if(!se_toast_popup_message.className) { se_toast_popup_message.className = 'se-toast-popup-message'; se_toast_popup_message.setAttribute('role', 'alert'); se_toast_popup_content.append(se_toast_popup_message); }
-        se_toast_popup_message.innerText = `글자수 : ${contentLengthTxt}자 (공백제외: ${contentLengthTrimTxt}자)`;
+        se_toast_popup_message.innerText = `글자수 : ${se.contentLength}자 (공백제외: ${se.contentLengthTrim}자)`;
         //container.__toast_timer = clearTimeout(container.__toast_timer);
         //container.__toast_timer = setTimeout(() => container.removeChild(se_toast_popup), 3000);
     }
@@ -145,12 +106,6 @@ head { display: block !important; }
     window.addEventListener('keyup', handler, false);
     window.addEventListener('keydown', handler, false);
     window.addEventListener('keypress', handler, false);
-    // document.addEventListener('click', handler_click, false);
+    window.addEventListener('click', handler_click, false);
     handler();
-}
-function _requestIdleCallback(callback) {
-    if(typeof requestIdleCallback == 'undefined') return setTimeout(callback, 1000);
-    return requestIdleCallback(callback);
-}
-function checkForDOM() { return (document.body) ? main() : _requestIdleCallback(checkForDOM); }
-_requestIdleCallback(checkForDOM);
+});
