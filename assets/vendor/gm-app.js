08386f65824006b51d107b917a73c2b573eb3209
Choi Won <help@myso.kr>
2021-06-10 05:51:49 +0900

    no message

--- assets/vendor/gm-app.js
+++ assets/vendor/gm-app.js
@@ -1,10 +1,15 @@
 (function(window) {
-  window.GM_App = function(callback) {
+  window.GM_App = function(callback, preload) {
     function _requestIdleCallback(callback) {
         if(typeof requestIdleCallback == 'undefined') return setTimeout(callback, 1000);
         return requestIdleCallback(callback);
     }
-    function checkForDOM() { return (document.body) ? callback() : _requestIdleCallback(checkForDOM); }
+    function checkForDOM() {
+      let container = document.body;
+      if(preload == 1) container = document.head;
+      if(preload == 2) container = document.documentElement;
+      return container ? callback() : _requestIdleCallback(checkForDOM);
+    }
     _requestIdleCallback(checkForDOM);
   }
 })(window);
