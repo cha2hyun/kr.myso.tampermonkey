0b75e77b210845cb5f7faea0ac3b74149f47fb0b
Choi Won <help@myso.kr>
2021-06-11 16:15:11 +0900

    no message

--- service/com.instagram-bypass.rightclick.user.js
+++ service/com.instagram-bypass.rightclick.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         인스타그램 이미지 우클릭 제한 해제
 // @namespace    https://tampermonkey.myso.kr/
-// @version      1.0.8
+// @version      1.0.9
 // @updateURL    https://github.com/myso-kr/kr.myso.tampermonkey/raw/master/service/com.instagram-bypass.rightclick.user.js
 // @description  인스타그램에서 이미지의 우클릭 기능이 활성화됩니다.
 // @author       Won Choi
@@ -10,18 +10,16 @@
 // @match        *://www.instagram.com
 // @match        *://www.instagram.com/*
 // @grant        GM_addStyle
-// @require      https://tampermonkey.myso.kr/assets/donation.js?v=3
 // ==/UserScript==
 async function main() {
-    GM_donation('#react-root section', 1);
-    GM_addStyle(`
-      ._9AhH0 { display: none; }
-      .PyenC, .fXIG0 { margin: auto; left: 0; right: 0; top: 0; bottom: 0; width: 135px; height: 135px; }
-   `);
+  GM_addStyle(`
+    ._9AhH0 { display: none; }
+    .PyenC, .fXIG0 { margin: auto; left: 0; right: 0; top: 0; bottom: 0; width: 135px; height: 135px; }
+ `);
 }
 function _requestIdleCallback(callback) {
-  if(typeof requestIdleCallback == 'undefined') return setTimeout(callback, 1000);
-  return requestIdleCallback(callback);
+if(typeof requestIdleCallback == 'undefined') return setTimeout(callback, 1000);
+return requestIdleCallback(callback);
 }
 function checkForDOM() { return (document.body) ? main() : _requestIdleCallback(checkForDOM); }
 _requestIdleCallback(checkForDOM);
