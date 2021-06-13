943435e1305096f4e94870e3943638e898247477
Choi Won <help@myso.kr>
2021-06-13 21:14:08 +0900

    no message

--- service/com.naver.blog-write.text.counter.user.js
+++ service/com.naver.blog-write.text.counter.user.js
@@ -1,11 +1,14 @@
 // ==UserScript==
 // @name         네이버 블로그&포스트 글자수 세기
 // @namespace    https://tampermonkey.myso.kr/
-// @version      1.1.10
+// @version      1.1.11
 // @updateURL    https://github.com/myso-kr/kr.myso.tampermonkey/raw/master/service/com.naver.blog-write.text.counter.user.js
 // @description  네이버 블로그&포스트에서 글자수 세기를 활성화합니다.
 // @author       Won Choi
 // @match        *://blog.naver.com/*/*
+// @match        *://blog.naver.com/*/postwrite*
+// @match        *://blog.naver.com/*Redirect=Write*
+// @match        *://blog.naver.com/*Redirect=Update*
 // @match        *://blog.naver.com/PostWriteForm*
 // @match        *://blog.naver.com/PostUpdateForm*
 // @match        *://blog.naver.com/PostView*
