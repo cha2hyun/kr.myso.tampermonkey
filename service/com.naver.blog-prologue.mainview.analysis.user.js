1fb761ff2b225c888315b5f473501e6a31fb912b
Choi Won <help@myso.kr>
2021-06-16 01:48:24 +0900

    no message

--- service/com.naver.blog-prologue.mainview.analysis.user.js
+++ service/com.naver.blog-prologue.mainview.analysis.user.js
@@ -16,7 +16,7 @@
 // @require      https://tampermonkey.myso.kr/assets/vendor/gm-xmlhttp-request-cors.js
 // @require      https://tampermonkey.myso.kr/assets/donation.js?v=210613
 // @require      https://tampermonkey.myso.kr/assets/lib/naver-blog.js
-// @require      https://tampermonkey.myso.kr/assets/lib/naver-main.js?v=210617
+// @require      https://tampermonkey.myso.kr/assets/lib/naver-main.js?v=21061604
 // @require      https://cdnjs.cloudflare.com/ajax/libs/bluebird/3.7.2/bluebird.min.js
 // @require      https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.21/lodash.min.js
 // @require      https://cdnjs.cloudflare.com/ajax/libs/toastify-js/1.11.0/toastify.min.js
@@ -27,13 +27,13 @@ GM_App(async function main() {
   const uri = new URL(location.href), params = Object.fromEntries(uri.searchParams.entries());
   const blogId = params.blogId; if(!blogId) return;
   (async function repeat() {
-      Toastify({ text: `메인 노출 데이터 가져오는 중...`, }).showToast();
+      Toastify({ text: `메인노출 데이터 가져오는 중...`, }).showToast();
       const items = await NM_searchAll();
       const finds = items.filter(o=>o.url.includes('blog.naver.com') && o.url.includes(`/${blogId}/`));
       if(finds.length) {
-          finds.map((item)=>Toastify({ text: `[${item.label}] ${item.url}`, onClick: ()=>window.open(item.url) }).showToast());
+          finds.map((item)=>Toastify({ text: `[메인노출:${item.label}] ${item.url}`, onClick: ()=>window.open(item.url) }).showToast());
       } else {
-          Toastify({ text: `메인 노출 데이터 없음`, }).showToast();
+          Toastify({ text: `메인노출 데이터 없음`, }).showToast();
       }
       setTimeout(repeat, 1000 * 60 * 5);
   })();
