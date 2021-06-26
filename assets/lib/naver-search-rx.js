3edf72ec9a70f1f7796d0b809bd30eb081b000ec
Choi Won <help@myso.kr>
2021-06-27 07:06:07 +0900

    no message

--- assets/lib/naver-search-rx.js
+++ assets/lib/naver-search-rx.js
@@ -80,7 +80,7 @@
     }
     window.NR_termsAll = async function NR_termsParagraph(...keywords) {
         const uniqs = keywords.filter((word, index, keywords)=>keywords.indexOf(word) == index);
-        const terms = []; for(let item of uniqs) { terms.push(...await NR_terms(item)); }
+        const terms = []; for(let item of uniqs) { terms.push(await NR_terms(item)); }
         return terms.flat();
         // return uniqs.length ? (await Promise.all(uniqs.map(NR_terms))).flat() : [];
     }
