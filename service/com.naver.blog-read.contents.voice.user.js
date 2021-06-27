e4573fc6d5a45f37a3c95353ce50265950678b31
Choi Won <help@myso.kr>
2021-06-28 07:16:19 +0900

    no message

--- service/com.naver.blog-read.contents.voice.user.js
+++ service/com.naver.blog-read.contents.voice.user.js
@@ -22,6 +22,30 @@
 GM_App(async function main() {
     GM_donation('#viewTypeSelector, #postListBody, #wrap_blog_rabbit, #writeTopArea, #editor_frame', 0);
     GM_addStyle(``);
+    GM_addScript(() => {
+        window.bgm = document.querySelector('#frameBGM') || document.createElement('div'); (document.body || document.documentElement).append(window.bgm);
+        window.bgm.id = 'frameBGM';
+        window.bgm.setAttribute('frameborder', 0);
+        window.bgm.setAttribute('style', 'width: 120px; height: 120px; position: fixed; left: 15px; bottom: 15px; opacity: 0.9; border-radius: 50rem; pointer-events: none; display: none;');
+        window.onYouTubeIframeAPIReady = function onYouTubeIframeAPIReady() {
+            window.yt = new YT.Player('frameBGM', {
+                events: { onReady(event){ window.yt.setVolume(25); } },
+                height: 120, width: 120,
+                playerVars: {
+                  listType: 'playlist', list: 'PLRBp0Fe2GpglTnOLbhyrHAVaWsCIEX53Y',
+                  autoplay: 0, loop: 1, rel: 0, index: Math.floor(Math.random() * 500),
+                },
+            });
+            window.bgm = document.querySelector('#frameBGM');
+        }
+    });
+    GM_addScript('https://www.youtube.com/iframe_api');
+    function bg_play() {
+        GM_addScript(()=>(window.yt && window.yt.playVideo(), window.bgm && (window.bgm.style.display = 'block')));
+    }
+    function bg_stop() {
+        GM_addScript(()=>(window.yt && window.yt.stopVideo(), window.bgm && (window.bgm.style.display = 'none')));
+    }
     function handler(event) {
         const wrappers = Array.from(document.querySelectorAll('[data-post-editor-version]'));
         wrappers.map((wrapper) => {
@@ -30,68 +54,74 @@ GM_App(async function main() {
             item.innerText = '오디오 블로그';
             item.onclick = async function(event) {
                 event.preventDefault();
-                if(GM_speechState()) return GM_speechReset();
-                const se = SE_parse(wrapper); if(!se || !se.content) return synth.speech('본문 정보를 인식 할 수 없습니다.');
-                for(let item of se.sections) {
-                    if(item.type == 'title') {
-                        for(let text of item.text) await GM_speech(text);
-                    }
-                    if(item.type == 'text') {
-                        for(let text of item.text) await GM_speech(text);
-                    }
-                    if(item.type == 'image') {
-                        for(let text of item.description) await GM_speech(`첨부된 이미지에 대한 설명입니다. ${text}.`);
-                    }
-                    if(item.type == 'video') {
-                        await GM_speech('첨부된 영상에 대한 설명입니다.');
-                        for(let text of item.title) await GM_speech(text);
-                        for(let text of item.description) await GM_speech(text);
-                    }
-                    if(item.type == 'line') {
-                        await GM_speech('잠시 후 네이버 블로그 오디오 리더가 이어서 재생됩니다.', 3000);
-                    }
-                    if(item.type == 'sticker') {}
-                    if(item.type == 'quotation') {
-                        for(let text of item.title) await GM_speech(text);
-                        for(let text of item.description) await GM_speech(text);
-                    }
-                    if(item.type == 'places') {
-                        for(let location of item.location) {
-                            const items = _.zip(location.name, location.addr);
-                            for(let item of items) await GM_speech(`첨부된 장소 ${item[0]}의 주소는 ${item[1]}입니다.`);
+                if(GM_speechState()) {
+                    GM_speechReset();
+                    bg_stop();
+                } else {
+                    const se = SE_parse(wrapper);
+                    bg_play();
+                    for(let item of se.sections) {
+                        if(item.type == 'title') {
+                            for(let text of item.text) await GM_speech(text);
+                        }
+                        if(item.type == 'text') {
+                            for(let text of item.text) await GM_speech(text);
+                        }
+                        if(item.type == 'image') {
+                            for(let text of item.description) await GM_speech(`첨부된 이미지에 대한 설명입니다. ${text}.`);
+                        }
+                        if(item.type == 'video') {
+                            await GM_speech('첨부된 영상에 대한 설명입니다.');
+                            for(let text of item.title) await GM_speech(text);
+                            for(let text of item.description) await GM_speech(text);
+                        }
+                        if(item.type == 'line') {
+                            await GM_speech('잠시 후 네이버 블로그 오디오 리더가 이어서 재생됩니다.', 3000);
+                        }
+                        if(item.type == 'sticker') {}
+                        if(item.type == 'quotation') {
+                            for(let text of item.title) await GM_speech(text);
+                            for(let text of item.description) await GM_speech(text);
+                        }
+                        if(item.type == 'places') {
+                            for(let location of item.location) {
+                                const items = _.zip(location.name, location.addr);
+                                for(let item of items) await GM_speech(`첨부된 장소 ${item[0]}의 주소는 ${item[1]}입니다.`);
+                            }
+                        }
+                        if(item.type == 'link') {
+                            const items = _.zip(item.title, item.description, item.hostname);
+                            for(let item of items) {
+                                await GM_speech(`첨부된 웹문서 "${item[0]}"에 대한 설명입니다. ${item[1]}. 이 웹문서는 ${item[2]} 웹사이트로 이동합니다.`);
+                            }
+                        }
+                        if(item.type == 'file') {
+                            for(let text of item.name) await GM_speech(`${text} 파일이 첨부되어 있습니다.`);
+                        }
+                        if(item.type == 'schedule') {
+                            await GM_speech('네이버 블로그 오디오 리더가 지원하지 않는 일정이 포함되어 있습니다.', 1500);
+                        }
+                        if(item.type == 'table' && item.table) {
+                            await GM_speech('네이버 블로그 오디오 리더가 지원하지 않는, 표가 포함되어 있습니다.', 1500);
+                        }
+                        if(item.type == 'code') {
+                            await GM_speech('네이버 블로그 오디오 리더가 지원하지 않는, 코드 내용이 포함되어 있습니다.', 1500);
+                        }
+                        if(item.type == 'formula') {
+                            await GM_speech('네이버 블로그 오디오 리더가 지원하지 않는, 수식 정보가 포함되어 있습니다.', 1500);
+                        }
+                        if(item.type == 'talktalk') {
+                            await GM_speech('궁금할 땐 네이버 톡톡하세요!');
+                        }
+                        if(item.type == 'material') {
+                            await GM_speech('네이버 블로그 오디오 리더가 지원하지 않는, 글감 정보가 포함되어 있습니다.', 1500);
                         }
                     }
-                    if(item.type == 'link') {
-                        const items = _.zip(item.title, item.description, item.hostname);
-                        for(let item of items) {
-                            await GM_speech(`첨부된 웹문서 "${item[0]}"에 대한 설명입니다. ${item[1]}. 이 웹문서는 ${item[2]} 웹사이트로 이동합니다.`);
-                        }
-                    }
-                    if(item.type == 'file') {
-                        for(let text of item.name) await GM_speech(`${text} 파일이 첨부되어 있습니다.`);
-                    }
-                    if(item.type == 'schedule') {
-                        await GM_speech('네이버 블로그 오디오 리더가 지원하지 않는 일정이 포함되어 있습니다.', 1500);
-                    }
-                    if(item.type == 'table' && item.table) {
-                        await GM_speech('네이버 블로그 오디오 리더가 지원하지 않는, 표가 포함되어 있습니다.', 1500);
-                    }
-                    if(item.type == 'code') {
-                        await GM_speech('네이버 블로그 오디오 리더가 지원하지 않는, 코드 내용이 포함되어 있습니다.', 1500);
-                    }
-                    if(item.type == 'formula') {
-                        await GM_speech('네이버 블로그 오디오 리더가 지원하지 않는, 수식 정보가 포함되어 있습니다.', 1500);
-                    }
-                    if(item.type == 'talktalk') {
-                        await GM_speech('궁금할 땐 네이버 톡톡하세요!');
-                    }
-                    if(item.type == 'material') {
-                        await GM_speech('네이버 블로그 오디오 리더가 지원하지 않는, 글감 정보가 포함되어 있습니다.', 1500);
-                    }
+                    await GM_speech('이상. 모든 글의 읽기가 완료되었습니다.', 1000);
+                    await GM_speech('네이버 블로그 오디오 리더가 마음에 드셨다면, 개발자 최원을 후원해주세요. 이용해 주셔서 감사합니다.');
+                    await GM_speechReset();
+                    bg_stop();
                 }
-                await GM_speech('이상. 모든 글의 읽기가 완료되었습니다.', 1000);
-                await GM_speech('네이버 블로그 오디오 리더가 마음에 드셨다면, 개발자 최원을 후원해주세요. 이용해 주셔서 감사합니다.');
-                await GM_speechReset();
             }
         });
     }
