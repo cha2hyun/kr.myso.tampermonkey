a4ecbab35e4890ee4e249468fde9d635bff90067
Choi Won <help@myso.kr>
2021-06-27 09:15:34 +0900

    no message

--- assets/lib/naver-blog.js
+++ assets/lib/naver-blog.js
@@ -19,7 +19,7 @@
     }
     window.NB_blogPostList = async function NB_blogPostList(blogId, limit = 10, options = {}) {
         let posts = [];
-        for(let currentPage = 1; currentPage <= limit; currentPage++) {
+        for(let currentPage = 1; currentPage < limit; currentPage++) {
             const data = await NB_blogInfo(blogId, 'PostListInfo', { ...options, currentPage }).catch(e=>({}));
             const list = data.postViewList; if(!list || !list.length) break;
             posts = posts.concat(list.filter(o=>o.categoryOpenYn && o.allOpenPost && o.searchYn));
