8227d85d733400cd2bfc5adcb52107f089d7dfa2
Choi Won <help@myso.kr>
2021-06-28 01:39:27 +0900

    no message

--- service/com.naver.blog-profile.keyword.counter.user.js
+++ service/com.naver.blog-profile.keyword.counter.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         네이버 블로그 보유키워드 분석
 // @namespace    https://tampermonkey.myso.kr/
-// @version      1.1.1
+// @version      1.1.2
 // @updateURL    https://github.com/myso-kr/kr.myso.tampermonkey/raw/master/service/com.naver.blog-profile.keyword.counter.user.js
 // @description  네이버 블로그 프로필에서 보유키워드를 확인할 수 있습니다.
 // @author       Won Choi
@@ -27,21 +27,22 @@ GM_App(async function main() {
     GM_donation('#post-area', 0);
     GM_addStyle(`@import url(https://cdnjs.cloudflare.com/ajax/libs/toastify-js/1.11.0/toastify.min.css)`);
     GM_addStyle(`
-      .blog-keywords-total-loading { position: relative; }
-      .blog-keywords-total-loading:after { content: "데이터 불러오는 중..."; color: #fff; font-family:  Lato,"Helvetica Neue" ; font-weight: 200; font-size: 16px; position: absolute; width: 100%; height: 20px; line-height: 20px; left: 0; top: 0; background-color: #e74c3c; z-index: 1; }
-      .blog-keywords-total-loading:before { content: ""; position: absolute; background-color: #fbb034; top: -5px; left: 0px; height: 30px; width: 0px; z-index: 0; opacity: 1; transform-origin: 100% 0%; animation: loader3 10s ease-in-out infinite; }
-      @keyframes loader3{ 0%{width: 0px;} 70%{width: 100%; opacity: 1;} 90%{opacity: 0; width: 100%;} 100%{opacity: 0;width: 0px;} }
-      #profile h2.keyword_info { padding: 5px 0; font-family: 돋움; font-size: 12px; margin-top: 30px; }
-      #profile h2.keyword_info img.arw01 { margin: 0 7px 1px 5px; }
-      #profile ul.keyword_info { list-style: none; clear: both; border-bottom: 1px solid #ccc; }
-      #profile ul.keyword_info li.item { font-size: 12px; display: inline-block; padding: 0.2rem 0.5rem; margin-right: 0.5rem; margin-top: 0.25rem; margin-bottom: 0.25rem; background-color: #0abf53; border-radius: 50rem; color: #fff; }
-      #profile ul.keyword_info li.line { dispaly:block; width: 100%; height:1px; overflow:hidden; padding: 0; margin-right: 0; background: #ccc; }
-      #profile ul.keyword_info li.head { dispaly:block; width: 100%; padding: 5px 0; font-family: 돋움; font-size: 12px; font-weight: bold; }
+    .blog-keywords-total-loading { position: relative; }
+    .blog-keywords-total-loading::after { content: attr(data-message); color: #fff; font-family:  Lato,"Helvetica Neue" ; font-weight: 200; font-size: 16px; position: absolute; width: 100%; height: 20px; line-height: 20px; left: 0; top: 0; background-color: #e74c3c; z-index: 1; }
+    .blog-keywords-total-loading::before { content: ""; position: absolute; background-color: #fbb034; top: -5px; left: 0px; height: 30px; width: 0px; z-index: 0; opacity: 1; transform-origin: 100% 0%; animation: loader3 10s ease-in-out infinite; }
+    @keyframes loader3{ 0%{width: 0px;} 70%{width: 100%; opacity: 1;} 90%{opacity: 0; width: 100%;} 100%{opacity: 0;width: 0px;} }
+    #profile h2.keyword_info { padding: 5px 0; font-family: 돋움; font-size: 12px; margin-top: 30px; }
+    #profile h2.keyword_info img.arw01 { margin: 0 7px 1px 5px; }
+    #profile ul.keyword_info { list-style: none; clear: both; border-bottom: 1px solid #ccc; }
+    #profile ul.keyword_info li.item { font-size: 12px; display: inline-block; padding: 0.2rem 0.5rem; margin-right: 0.5rem; margin-top: 0.25rem; margin-bottom: 0.25rem; background-color: #0abf53; border-radius: 50rem; color: #fff; }
+    #profile ul.keyword_info li.line { dispaly:block; width: 100%; height:1px; overflow:hidden; padding: 0; margin-right: 0; background: #ccc; }
+    #profile ul.keyword_info li.head { dispaly:block; width: 100%; padding: 5px 0; font-family: 돋움; font-size: 12px; font-weight: bold; }
     `);
     const uri = new URL(location.href), { blogId } = Object.fromEntries(uri.searchParams.entries()); if(!blogId) return;
     const wrp = document.querySelector('#profile'); if(!wrp) return;
     const cnv = wrp.querySelector('.blog-keywords-total') || document.createElement('div'); cnv.classList.add('blog-keywords-total'); wrp.append(cnv);
     (async function redraw(categoryNo, pages = Number.MAX_SAFE_INTEGER) {
+        redraw.count = 0;
         const blog = {};
         blog.BlogInfo = await NB_blogInfo(blogId, 'BlogInfo');
         blog.BlogUserInfo = await NB_blogInfo(blogId, 'BlogUserInfo');
@@ -50,8 +51,8 @@ GM_App(async function main() {
         blog.TalkTalkAndReservationInfo = await NB_blogInfo(blogId, 'TalkTalkAndReservationInfo');
         const postsSizes = _.get(blog, 'CategoryList.mylogPostCount', 1);
         const categories = _.get(blog, 'CategoryList.mylogCategoryList', []).filter(o=>o.openYN && o.postCnt > 0);
-        const posts = (categoryNo !== undefined) ? await NB_blogPostList(blogId, pages, { categoryNo }) : [];
-        const posts_with_terms = await Promise.mapSeries(posts, async (post)=>(post.terms = await NX_termsParagraph(post.titleWithInspectMessage), post));
+        const posts = ((cnv.dataset.message = `콘텐츠 가져오는 중... 모든 작업을 중단하고 완료가 될 때까지 가만히 기다려 주세요...`) && categoryNo !== undefined) ? await NB_blogPostList(blogId, pages, { categoryNo }) : [];
+        const posts_with_terms = await Promise.mapSeries(posts, async (post)=>(cnv.dataset.message = `콘텐츠 분석 중... (${++redraw.count}/${posts.length})`, post.terms = await NX_termsParagraph(post.titleWithInspectMessage), post));
         const terms = posts_with_terms.map((post)=>post.terms).flat();
         const uniqs = terms.filter((word, index, keywords)=>keywords.indexOf(word) == index);
         const group = uniqs.reduce((group, query, index)=>(group[index] = Object.assign({ query, count: terms.filter(item=>item==query).length }), group), []).sort((a, b)=>b.count - a.count);
